19c2a87d2c9d14e3461ba29802e13d168d94a8de
Ployrut-Thongtub <Ployrut1205@gmail.com>
2026-02-13 01:04:54 +0700

    98

--- main.go
+++ main.go
@@ -187,14 +187,14 @@ func main() {
 		&models.PermissionGroup{},
 		&models.Permission{},
 
-		&models.ActivityGoal{},
-		&models.ActivitySubGoal{},
-		&models.ActivityMainCategory{},
-		&models.ActivitySubCategory{},
+		// &models.ActivityGoal{},
+		// &models.ActivitySubGoal{},
+		// &models.ActivityMainCategory{},
+		// &models.ActivitySubCategory{},
 
-		// 4. สองตัวนี้อยู่ล่างสุดเสมอ เพราะต้องรอ User และ Activity
-		&models.UserFavorite{},
-		&models.UserReadHistory{},
+		// // 4. สองตัวนี้อยู่ล่างสุดเสมอ เพราะต้องรอ User และ Activity
+		// &models.UserFavorite{},
+		// &models.UserReadHistory{},
 	)
 
 	if err != nil {
